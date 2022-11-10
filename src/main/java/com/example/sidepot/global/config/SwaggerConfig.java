58840f1ab7ca9ad8f82d9e032b2fae1dbb789e9c
장수현 <76679995+suhyeon10@users.noreply.github.com>
2022-11-11 00:50:10 +0900

    config : swagger-ui

---
+++ src/main/java/com/example/sidepot/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.sidepot.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableWebMvc
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("sidepot.com"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("dev Rest API")
+                .version("v1.0")
+                .description("근무시간 어플리케이션 swagger api")
+                .build();
+    }
+
+}
