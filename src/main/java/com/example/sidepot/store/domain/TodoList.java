b4a422ce920825414df1e6191cdcc59be2e5bbb5
장수현 <76679995+suhyeon10@users.noreply.github.com>
2023-01-04 18:47:24 +0900

    init

--- src/main/java/com/example/sidepot/store/domain/TodoList.java
+++ src/main/java/com/example/sidepot/store/domain/TodoList.java
@@ -1,2 +1,22 @@
-package com.example.sidepot.store.domain;public class TodoList {
+package com.example.sidepot.store.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "todo_list")
+@NoArgsConstructor
+public class TodoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany()
+    private List<String> todoListDetail = new ArrayList<>();
+
 }
--- src/main/java/com/example/sidepot/store/domain/TodoListDetail.java
+++ src/main/java/com/example/sidepot/store/domain/TodoListDetail.java
@@ -1,2 +1,14 @@
-package com.example.sidepot.store.domain;public class TodoListDetail {
+package com.example.sidepot.store.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class TodoListDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String todoListDetail;
 }
