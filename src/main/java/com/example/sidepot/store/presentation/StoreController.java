cd74f83a8f58708bcd4ccf50675d06455f73a1f6
장수현 <76679995+suhyeon10@users.noreply.github.com>
2023-01-04 17:35:30 +0900

    update : @restController

--- src/main/java/com/example/sidepot/store/presentation/StoreController.java
+++ src/main/java/com/example/sidepot/store/presentation/StoreController.java
@@ -7,24 +7,21 @@ import com.example.sidepot.store.dto.StoreCreateRequestDto;
 import com.example.sidepot.store.dto.StoreResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 @Api(tags = "매장 관련 APIs")
 @RequestMapping(Path.REST_BASE_PATH)
 public class StoreController {
 
     private final StoreService storeService;
-
-    public StoreController(StoreService storeService) {
-        this.storeService = storeService;
-    }
 
     @GetMapping("/stores")
     @ApiOperation(value = "[매장관리] 1. 매장 리스트 조회", notes = "오너가 가진 모든 매장을 조회하는 API")
