e4c92ff5681cf0773f523f04dfab2d5574d35447
장수현 <76679995+suhyeon10@users.noreply.github.com>
2023-02-22 22:42:50 +0900

    feat : 주소 검색 html 추가

--- src/main/java/com/example/sidepot/global/config/WebMvcConfig.java
+++ src/main/java/com/example/sidepot/global/config/WebMvcConfig.java
@@ -5,16 +5,19 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @RequiredArgsConstructor
@@ -32,5 +35,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(APP_DATE_FORMAT)))
                 .build();
         converters.add(JACKSON_CONVERTER_SEQ, new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
 }
