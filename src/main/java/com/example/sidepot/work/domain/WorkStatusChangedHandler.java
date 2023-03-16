d65f4f2055b164436349d47ff9e0c7c67c30d2de
jjjjooo <fullbster@gmail.com>
2023-03-16 22:30:00 +0900

    [fix] WorkStatusChangedHandler bean 못찾는 문제

--- src/main/java/com/example/sidepot/work/domain/WorkStatusChangedHandler.java
+++ src/main/java/com/example/sidepot/work/domain/WorkStatusChangedHandler.java
@@ -1,7 +1,5 @@
 package com.example.sidepot.work.domain;
 
-import com.example.sidepot.work.app.AttendanceService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -9,10 +7,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class WorkStatusChangedHandler {
-    private final AttendanceService attendanceService;
 
     @EventListener(WorkStatusChangedHandler.class)
-    public void handle(WorkStatusChangedEvent event){
-        attendanceService.changeWorkStatus();
-    }
+    public void handle(WorkStatusChangedEvent event){}
 }
